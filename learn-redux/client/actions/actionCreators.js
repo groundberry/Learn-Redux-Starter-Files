b835a0e5f5a053da663bba75e81773bb26624767
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2018-02-20 18:57:11 -0800

    Create Redux Actions

---
+++ learn-redux/client/actions/actionCreators.js
@@ -0,0 +1,24 @@
+// Increment
+export function increment(index) {
+ return {
+   type: 'INCREMENT_LIKES',
+   index
+ }
+}
+
+//  Add comment
+export function addComment(postId, author, comment) {
+ return {
+   type: 'ADD_COMMENT',
+   postId,
+   author,
+   comment
+ }
+}
+
+//  Remove comment
+export function removeComment(postId, i) {
+ type: 'REMOVE_COMMENT',
+ i,
+ postId
+}
